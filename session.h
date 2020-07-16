04d07f88471873f72976944e3762b8e3fd22ad15
falkTX <falktx@falktx.com>
2020-07-16 12:48:41 +0100

    Cleanup whitespace
    Signed-off-by: falkTX <falktx@falktx.com>

--- session.h
+++ session.h
@@ -2,17 +2,17 @@
     Copyright (C) 2001 Paul Davis
     Copyright (C) 2004 Jack O'Quin
     Copyright (C) 2010 Torben Hohn
-    
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation; either version 2.1 of the License, or
     (at your option) any later version.
-    
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
-    
+
     You should have received a copy of the GNU Lesser General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -168,7 +168,7 @@ typedef void (*JackSessionCallback)(jack_session_event_t *event,
  *
  * setting more than one session_callback per process is probably a design
  * error. if you have a multiclient application its more sensible to create
- * a jack_client with only a session callback set. 
+ * a jack_client with only a session callback set.
  *
  * @return 0 on success, otherwise a non-zero error code
  */
