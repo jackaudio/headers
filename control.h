b601e3dc285890db754038dcb7994ea5bc619fd1
David Robillard <d@drobilla.net>
2022-08-22 14:19:40 -0400

    Use a consistent style for copyright headers
    I chose the most common one here, although the indentation is a bit odd.

--- control.h
+++ control.h
@@ -1,24 +1,21 @@
-/* -*- Mode: C ; c-basic-offset: 4 -*- */
 /*
-  JACK control API
+    Copyright (C) 2008 Nedko Arnaudov
+    Copyright (C) 2008 GRAME
 
-  Copyright (C) 2008 Nedko Arnaudov
-  Copyright (C) 2008 GRAME
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2 of the License.
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; version 2 of the License.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
+
 /**
  * @file   jack/control.h
  * @ingroup publicheader
